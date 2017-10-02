f4bcaf41e8040be41f616f0824ba0ba2c99ca96d
Breno Brandão <breno.brandao@celeritech.com.br>
2017-10-02 15:44:24 -0300

    Junção dos arquivos em apenas 1 .lua

--- tarefas/tarefa-03/main.lua
+++ tarefas/tarefa-03/main.lua
@@ -1,5 +1,3 @@
-require "tank"
-
 function love.load()
 	love.physics.setMeter(32)
 	world = love.physics.newWorld(0, 0, true)
@@ -39,4 +37,103 @@ function love.draw()
 		love.graphics.setColor(238, 238, 238)
 		love.graphics.circle("fill", objects.tank1.physic.body:getX(), objects.tank1.physic.body:getY(), objects.tank1.physic.shape:getRadius())
 	end
+end
+
+Tank = {
+	actualdirection = "up",
+	shooting = false,
+	lastShotTime = nil,
+
+	physic = {},
+
+	up = "up",
+	down = "down",
+	left = "left",
+	right = "right",
+	space = "space"
+}
+
+function Tank:setControllers( up, down, left, right, space )
+	self.up = up
+	self.down = down
+	self.left = left
+	self.right = right
+	self.space = space
+end
+
+function Tank:setDirection( actualdirection )
+	self.actualdirection = actualdirection
+end
+
+function Tank:setShooting( shooting )
+	self.shooting = shooting
+end
+
+function Tank:isShooting()
+	return self.shooting
+end
+
+--function Tank:setBody( body )
+--	self.body = body
+--end
+--
+--function Tank:setShape( shape )
+--	self.shape = shape
+--end
+--
+--function Tank:setFixture( fixture )
+--	self.fixture = fixture
+--end
+
+function Tank:readMovement( dt )
+	if self.up == nil or self.down == nil or self.left == nil or self.right == nil then return end
+	uppressed = false
+	downpressed = false
+	leftpressed = false
+	rightpressed = false
+	if love.keyboard.isDown(self.up) then
+		uppressed = true
+		self.physic.body:setY(self.physic.body:getY() - defaultlinearvelocity*dt)
+    elseif love.keyboard.isDown(self.down) then
+		downpressed = true
+		self.physic.body:setY(self.physic.body:getY() + defaultlinearvelocity*dt)
+    end
+	if love.keyboard.isDown(self.right) then
+		rightpressed = true
+		self.physic.body:setX(self.physic.body:getX() + defaultlinearvelocity*dt)
+    elseif love.keyboard.isDown(self.left) then
+		leftpressed = true
+		self.physic.body:setX(self.physic.body:getX() - defaultlinearvelocity*dt)
+    end
+
+    if uppressed == true then
+    	self.actualdirection = "up"
+    	if leftpressed == true then
+    		self.actualdirection = "upleft"
+    	elseif rightpressed == true then
+    		self.actualdirection = "upright"
+    	end
+    elseif downpressed == true then
+    	self.actualdirection = "down"
+    	if leftpressed == true then
+    		self.actualdirection = "downleft"
+    	elseif rightpressed == true then
+    		self.actualdirection = "downright"
+    	end
+    elseif rightpressed == true then
+    	self.actualdirection = "right"
+    elseif leftpressed == true then
+    	self.actualdirection = "left"
+    end
+end
+
+function Tank:readShootingState()
+	if love.keyboard.isDown(self.space) and love.timer.getTime() - self.lastShotTime > 1 then
+		shooting = true
+    end
+end
+
+function Tank:update( dt )
+	self:readMovement(dt)
+	self:readShootingState()
 end
